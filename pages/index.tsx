c16f22d24185ecb6d8bc010178ebfa0f4695705d
irenekyc <gugu_irene@hotmail.com>
2022-05-26 22:47:36 +0800

    fixed test script to avoid watching test result for pre commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import Button from '../components/button'
 import { BUTTON_VARIANT_OUTLINE } from '../constants/button'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Heading heading={1}>Hello World</Heading>
-      <Button variant={BUTTON_VARIANT_OUTLINE}>Outline</Button>
-    </div>
-  )
+	return (
+		<div>
+			<Heading heading={1}>Hello World</Heading>
+			<Button variant={BUTTON_VARIANT_OUTLINE}>Outline</Button>
+		</div>
+	)
 }
 
 export default Home
