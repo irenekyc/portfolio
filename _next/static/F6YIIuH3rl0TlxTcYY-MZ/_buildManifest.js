9380f3ddc0ac901f5d86e352973f180a89c0406b
irenekyc <gugu_irene@hotmail.com>
2022-05-31 23:54:07 +0800

    Deploy gh-pages

---
+++ _next/static/F6YIIuH3rl0TlxTcYY-MZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/151-1868872d8b289c88.js","static/css/7a649ef4429ad856.css","static/chunks/pages/index-72e2cdbc807ec237.js"],"/_error":["static/chunks/pages/_error-4e2d7238c5614872.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
